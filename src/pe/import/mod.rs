2eba32d1d4d03084603a95fa60451e6ecb01d30b
sunilkr <badboy16a@gmail.com>
2022-09-25 01:02:36 +0530

    Refactored import parsing

--- src/pe/import/mod.rs
+++ src/pe/import/mod.rs
@@ -3,7 +3,12 @@ use chrono::{DateTime, Utc, NaiveDateTime};
 
 use crate::{types::{HeaderField, Header}, utils::Reader, Result};
 use std::{io::{Cursor, BufReader}, fmt::Display, mem::size_of, fs::File};
+use self::{x86::ImportLookup32, x64::ImportLookup64};
+
 use super::{section::{SectionTable, offset_to_rva, rva_to_offset, self, BadOffsetError, BadRvaError}, optional::ImageType};
+
+mod x86;
+mod x64;
 
 #[derive(Debug, Default)]
 pub struct ImportName {
@@ -17,135 +22,6 @@ impl Display for ImportName {
     }
 }
 
-#[derive(Debug, Default)]
-pub struct ImportLookup32 {
-    pub value: HeaderField<u32>,
-    pub is_ordinal: bool,
-    pub ordinal: Option<u16>,
-    pub iname: Option<HeaderField<ImportName>>,
-}
-
-impl Display for ImportLookup32 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {        
-        if self.is_ordinal {
-            write!(f, "{}", self.ordinal.unwrap_or(u16::MAX))
-        }
-        else {
-            let name = if let Some(name_hdr) = &self.iname {
-                format!("{}", name_hdr.value)
-            }
-            else {
-                String::from("ERR")
-            };
-
-            write!(f, "{}", name)
-        }
-    }
-}
-
-impl ImportLookup32 {
-    pub fn new(value: HeaderField<u32>) -> Self {
-        let val = value.value;
-        let is_ordinal = (val & (1<<31)) != 0;
-        let mut ordinal = None;
-        let mut name = None;
-
-        if is_ordinal {
-            ordinal = Some(val as u16);
-        }
-        else {
-            let iname_rva = (val as u32) & 0x7FFFFFFF;
-            name = Some(HeaderField{value: Default::default(), offset: 0, rva: iname_rva as u64});
-        }
-
-        Self { 
-            value: value, 
-            is_ordinal: is_ordinal,
-            ordinal: ordinal,
-            iname: name,
-        }
-    }
-
-    pub fn update_name(&mut self, sections: &SectionTable, reader: &mut dyn Reader) -> crate::Result<()> {
-        if let Some(iname) = &mut self.iname {
-            let offset = section::rva_to_offset(sections, iname.rva as u32).ok_or(section::BadRvaError(iname.rva))?;
-            let hint = reader.read_bytes_at_offset(offset.into(), 2)?;
-            let hint = LittleEndian::read_u16(&hint);
-            let name = reader.read_string_at_offset((offset+2).into())?;
-            iname.offset = offset.into();
-            iname.value = ImportName {
-                hint: HeaderField { value: hint, offset: offset.into(), rva: iname.rva },
-                name: HeaderField { value: name, offset: (offset+2).into(), rva: iname.rva+2 }
-            };
-        }
-        Ok(())
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct ImportLookup64 {
-    pub value: HeaderField<u64>,
-    pub is_ordinal: bool,
-    pub ordinal: Option<u16>,
-    pub iname: Option<HeaderField<ImportName>>,
-}
-
-impl Display for ImportLookup64 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {        
-        if self.is_ordinal {
-            write!(f, "{}", self.ordinal.unwrap_or(u16::MAX))
-        }
-        else {
-            let name = if let Some(name_hdr) = &self.iname {
-                format!("{}", name_hdr.value)
-            }
-            else {
-                String::from("ERR")
-            };
-
-            write!(f, "{}", name)
-        }
-    }
-}
-
-impl ImportLookup64 {
-    pub fn new(value: HeaderField<u64>) -> Self {
-        let val = value.value;
-        let is_ordinal = (val & (1<<63)) != 0;
-        let mut ordinal = None;
-        let mut name = None;
-
-        if is_ordinal {
-            ordinal = Some(val as u16);
-        }
-        else {
-            let iname_rva = (val as u32) & 0x7FFFFFFF;
-            name = Some(HeaderField{value: Default::default(), offset: 0, rva: iname_rva as u64});
-        }
-
-        Self { 
-            value: value, 
-            is_ordinal: is_ordinal,
-            ordinal: ordinal,
-            iname: name, 
-        }
-    }
-
-    pub fn update_name(&mut self, sections: &SectionTable, reader: &mut dyn Reader) -> crate::Result<()> {
-        if let Some(iname) = &mut self.iname {
-            let offset = section::rva_to_offset(sections, iname.rva as u32).ok_or(section::BadRvaError(iname.rva))?;
-            let hint = reader.read_bytes_at_offset(offset.into(), 2)?;
-            let hint = LittleEndian::read_u16(&hint);
-            let name = reader.read_string_at_offset((offset+2).into())?;
-            iname.offset = offset.into();
-            iname.value = ImportName {
-                hint: HeaderField { value: hint, offset: offset.into(), rva: iname.rva },
-                name: HeaderField { value: name, offset: (offset+2).into(), rva: iname.rva+2 }
-            };
-        }
-        Ok(())
-    }
-}
 
 #[derive(Debug)]
 pub enum ImportLookup {
---
+++ src/pe/import/x86.rs
@@ -0,0 +1,72 @@
+use std::fmt::Display;
+
+use byteorder::{LittleEndian, ByteOrder};
+
+use crate::{types::HeaderField, pe::section::{SectionTable, self}, utils::Reader};
+
+use super::ImportName;
+
+#[derive(Debug, Default)]
+pub struct ImportLookup32 {
+    pub value: HeaderField<u32>,
+    pub is_ordinal: bool,
+    pub ordinal: Option<u16>,
+    pub iname: Option<HeaderField<ImportName>>,
+}
+
+impl Display for ImportLookup32 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {        
+        if self.is_ordinal {
+            write!(f, "{}", self.ordinal.unwrap_or(u16::MAX))
+        }
+        else {
+            let name = if let Some(name_hdr) = &self.iname {
+                format!("{}", name_hdr.value)
+            }
+            else {
+                String::from("ERR")
+            };
+
+            write!(f, "{}", name)
+        }
+    }
+}
+
+impl ImportLookup32 {
+    pub fn new(value: HeaderField<u32>) -> Self {
+        let val = value.value;
+        let is_ordinal = (val & (1<<31)) != 0;
+        let mut ordinal = None;
+        let mut name = None;
+
+        if is_ordinal {
+            ordinal = Some(val as u16);
+        }
+        else {
+            let iname_rva = (val as u32) & 0x7FFFFFFF;
+            name = Some(HeaderField{value: Default::default(), offset: 0, rva: iname_rva as u64});
+        }
+
+        Self { 
+            value: value, 
+            is_ordinal: is_ordinal,
+            ordinal: ordinal,
+            iname: name,
+        }
+    }
+
+    pub fn update_name(&mut self, sections: &SectionTable, reader: &mut dyn Reader) -> crate::Result<()> {
+        if let Some(iname) = &mut self.iname {
+            let offset = section::rva_to_offset(sections, iname.rva as u32).ok_or(section::BadRvaError(iname.rva))?;
+            let hint = reader.read_bytes_at_offset(offset.into(), 2)?;
+            let hint = LittleEndian::read_u16(&hint);
+            let name = reader.read_string_at_offset((offset+2).into())?;
+            iname.offset = offset.into();
+            iname.value = ImportName {
+                hint: HeaderField { value: hint, offset: offset.into(), rva: iname.rva },
+                name: HeaderField { value: name, offset: (offset+2).into(), rva: iname.rva+2 }
+            };
+        }
+        Ok(())
+    }
+}
