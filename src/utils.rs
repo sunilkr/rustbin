76b5cbb87a6b3187483ef79d561a8863a2f7a737
sunilkr <badboy16a@gmail.com>
2023-04-18 22:05:09 +0530

    added ability to read wide char strings (for resources)

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,19 @@
 use std::{io::{Cursor, SeekFrom, Seek, BufRead, Read}, error::Error};
 
+use byteorder::{ReadBytesExt, LittleEndian};
+
 pub trait Reader {
-    fn read_string_at_offset(&mut self, offset: u64) -> Result<String, Box<dyn Error>>;
-    fn read_bytes_at_offset(&mut self, offset: u64, size: usize) -> Result<Vec<u8>, Box<dyn Error>>;
+    fn read_string_at_offset(&mut self, _offset: u64) -> Result<String, Box<dyn Error>>{
+        Ok(String::new())
+    }
+
+    fn read_bytes_at_offset(&mut self, _offset: u64, _size: usize) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(vec![0x00])
+    }
+
+    fn read_wchar_string_at_offset(&mut self, _offset: u64) -> Result<String, Box<dyn Error>> {
+        Ok(String::new())
+    }
 }
 
 pub struct ContentBase<'a> {
@@ -30,6 +41,14 @@ impl Reader for ContentBase<'_> {
         self.cursor.read_exact(&mut buf)?;
         Ok(buf)
     }
+
+    fn read_wchar_string_at_offset(&mut self, offset: u64) -> Result<String, Box<dyn Error>> {
+        self.cursor.seek( SeekFrom::Start(offset))?;
+        let len = self.cursor.read_u16::<LittleEndian>()?;
+        let mut buf = vec![0u16; len.into()];
+        self.cursor.read_u16_into::<LittleEndian>(&mut buf)?;
+        Ok(String::from_utf16(&buf)?)
+    }
 }
 
 pub fn read_string_at_offset(content: &[u8], offset: u64) -> Option<String> {
@@ -38,4 +57,19 @@ pub fn read_string_at_offset(content: &[u8], offset: u64) -> Option<String> {
     cursor.seek(SeekFrom::Start(offset)).unwrap();
     cursor.read_until(b'\0', &mut buf).unwrap();
     Some(String::from_utf8(buf[..(buf.len()-1)].to_vec()).unwrap())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::{ContentBase, Reader};
+
+    #[test]
+    fn test_read_wchar_string_at_offset() {
+        let mut reader = ContentBase::new(&[0x04u8, 0x00, 0x41, 0x00, 0x41, 0x00,0x41, 0x00, 0x41, 0x00]);
+        let str = reader.read_wchar_string_at_offset(0).unwrap();
+        assert_eq!(str, String::from_str("AAAA").unwrap());
+    }
 }
